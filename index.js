389c9acff992725d348ed38d3abd3ae6ede32519
Terrie Ann <malobaterryann@gmail.com>
2024-11-18 06:05:05 -0800

    functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Follow along with the examples here
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip(); 
+function doSomething(thing) {
+    console.log(thing);
+  }
+  
+  doSomething("Terry");
+  function add(x, y) {
+    return x + y;
+  }
+  console.log(add(1, 2));
